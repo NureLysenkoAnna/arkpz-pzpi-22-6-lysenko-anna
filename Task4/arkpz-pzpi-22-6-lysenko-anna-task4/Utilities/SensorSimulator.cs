ddb90801045105e4e5d51d0d2214d3f776154000
annalysenko <anech@DESKTOP-HNUPDC3>
2024-12-27 01:42:16 +0200

    Setting up the client's IoT, data generation, and business logic.

---
+++ Task4/arkpz-pzpi-22-6-lysenko-anna-task4/Utilities/SensorSimulator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTClient.Utilities
+{
+    /// <summary>
+    /// Статичний клас для імітації роботи сенсорів шляхом генерації випадкових даних.
+    /// </summary>
+    public static class SensorSimulator
+    {
+        private static Random _random = new Random();
+
+        public static double GenerateGasLevel() => Math.Round(_random.NextDouble() * 100, 2);
+        public static double GenerateTemperature() => Math.Round(_random.NextDouble() * 50, 2);
+        public static double GeneratePressure() => Math.Round(_random.NextDouble() * 10 + 1, 2);
+    }
+}
